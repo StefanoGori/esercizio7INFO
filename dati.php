230ca700e7190643ec8dda7088d91790bb9b1401
stefanogori <stefanogori19@gmail.com>
2021-11-13 19:45:50 +0100

    es finito

--- dati.php
+++ dati.php
@@ -7,16 +7,72 @@
         <h1>Gioco dei fiammiferi</h1>
         <?php
             session_start();
-            $volta=0;
-            $turno=0;
-            $nome=$_POST['nome']=$_SESSION['nome'];
-            $fiammiferi=13;
-            function removeU(){
-                $fiammiferi-=$fiammiferiU;
+            $nome=$_SESSION['nome'];
+            
+            if(!isset($_SESSION['fiammiferiU'])){
+                $_SESSION['fiammiferiU']=0;     
             }
-            function removeC(){
-                $fiammiferi-=$fiammiferiC;
+            if( $_SESSION['volta']==0){
+                $_SESSION['fiammiferi']=13;
+                $_SESSION['turno']=1;
+                $_SESSION['volta']++;
             }
+            else{
+                $_SESSION['fiammiferi']-=$_POST['fiammiferiU']+(4-$_POST['fiammiferiU']);
+                $_SESSION['turno']++;
+             }   
+            
+            $fiammiferi=$_SESSION['fiammiferi'];
+                echo"
+                        GIOCATORE:
+                        $nome
+                        <br>
+                        <br>
+                        TURNO:
+                        $_SESSION[turno]
+
+                ";
+                if($_SESSION['turno']>1){
+                    $fiammiferiC=4-$_POST['fiammiferiU'];
+                    echo"
+                        <br>
+                        <br>
+                        hai tolto:$_POST[fiammiferiU] fiammiferi
+                        <br>
+                        io tolgo:$fiammiferiC fiammiferi
+                        <br>
+                    ";
+                    
+                }
+                for($i=0;$i<$fiammiferi;$i++){
+                    echo"
+                        <img src=images/fiammiferi.jpg>
+                    ";
+                }
+
+                if($fiammiferi==1){
+                    echo"
+                    Spiacente hai perso
+                    <br>
+                    <form action=index.php method=POST>
+                        <input type=submit value=RIGIOCA>
+                    </form>
+                    ";
+                }
+                else{
+                    echo"
+                        <form action=dati.php method=POST>
+                            <label for=fiammiferiU>Scegli quanti ne vuoi togliere</label>
+                            <select name=fiammiferiU>
+                                <option value=1>1</option>
+                                <option value=2>2</option>
+                                <option value=3>3</option>
+                            </select>
+                            <br>
+                            <input type=submit value=TOGLI>
+                        </form>
+                    ";
+                }
         ?>
     </body>
 </html>
--- index.php
+++ index.php
@@ -7,10 +7,12 @@
         <h1>Gioco dei fiammiferi</h1>
         <?php
         session_start();    
+        $_SESSION['volta']=0;
+        $fiammiferi=$_SESSION['fiammiferi']=13;
             echo"
                 <form action=dati.php method=POST>
                     <label for=nome>Nome:</label>
-                    <input type=text>
+                    <input type=text name=nome>
                     <input type=submit value=GIOCA>
                 </form>
             "
